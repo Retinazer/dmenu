2834e487f1c7e7cdd7c45b67815f0b76d587c8d7
Retinazer <coleyoung@protonmail.com>
2020-07-25 21:39:39 -0400

    ugh

--- config.h
+++ config.h
@@ -7,16 +7,16 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Hack:size=11"
+	"Hack:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#d0d0d0", "#121212" },
-	[SchemeSel] = { "#121212", "#f6ad09" },
-	[SchemeSelHighlight] = { "#121212", "#fbed59" },
-	[SchemeNormHighlight] = { "#b93f68", "#121212" },
-	[SchemeOut] = { "#121212", "#f6ad09" },
+	[SchemeNorm] = { "#d0d0d0", "#1E252C" },
+	[SchemeSel] = { "#d0d0d0", "#2D3741" },
+	[SchemeSelHighlight] = { "#1a2026", "#2d3741" },
+	[SchemeNormHighlight] = { "#d0d0d0", "#1A2026" },
+	[SchemeOut] = { "#1a2026", "#1e252c" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
