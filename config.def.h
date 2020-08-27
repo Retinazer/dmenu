7def5d5d17f656d95d96e1bb43d242a4effe12d2
Retinazer <coleyoung@protonmail.com>
2020-08-26 21:47:50 -0400

    Themed for new dotfiles repo

--- config.def.h
+++ config.def.h
@@ -7,16 +7,16 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Montserrat:style=Regular:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#d0d0d0", "#1E252C" },
+	[SchemeSel] = { "#d0d0d0", "#2D3741" },
+	[SchemeSelHighlight] = { "#1a2026", "#2d3741" },
+	[SchemeNormHighlight] = { "#d0d0d0", "#1A2026" },
+	[SchemeOut] = { "#1a2026", "#1e252c" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
